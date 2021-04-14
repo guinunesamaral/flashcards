af345a5fddc46b701ee5925250153d17f8d8c08a
guilherme nunes <guinucode@protonmail.com>
2021-04-14 20:53:31 -0300

    I changed the main class (App) to get right into the flashcardsscene

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,8 @@
-import database.DatabaseManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.net.URL;
 import java.nio.file.Paths;
 
 public class App extends Application
@@ -13,14 +10,12 @@ public class App extends Application
     @Override
     public void start(Stage stage) throws Exception
     {
-        URL fxmlUrl = Paths.get("./src/main/java/fxml/application.fxml").toUri().toURL();
-        Parent root = FXMLLoader.load(fxmlUrl);
-        Scene scene = new Scene(root);
-
-        DatabaseManager db = new DatabaseManager();
+        FXMLLoader loginFXML = new FXMLLoader(Paths.get("./src/main/java/fxml/home.fxml").toUri().toURL());
+        Scene loginScene = new Scene(loginFXML.load());
 
         stage.setTitle("Flashcards");
-        stage.setScene(scene);
+        stage.setScene(loginScene);
+        stage.setResizable(false);
         stage.show();
     }
 
