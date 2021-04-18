83f2bec7fdf1641439b204f4c86b14568fa4a482
guilherme nunes <guinucode@protonmail.com>
2021-04-18 14:15:57 -0300

    Removed the flashcard logic from this controller and passed to a flashcard controller

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,32 +1,27 @@
 package controllers;
 
-import javafx.animation.RotateTransition;
+import database.XMLReader;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ToolBar;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 
 public class HomeController extends Controller
 {
+    public static final String NOTIFICATIONS_SCENE_PATH = "./src/main/java/screens/notifications.fxml";
+    public static final String USER_DATA_PATH = "./src/main/java/database/user-data.xml";
+    public static final String FLASHCARD_PATH = "./src/main/java/components/flashcard.fxml";
+
     @FXML
     public AnchorPane header;
     @FXML
@@ -34,117 +29,26 @@ public class HomeController extends Controller
     @FXML
     public Button exitBtn;
     @FXML
-    public FlowPane flashcardsBox;
+    public ChoiceBox flashcardsThemes;
+    @FXML
+    public FlowPane flashcardBox;
 
+    public XMLReader xmlReader;
     public String userId;
 
     @FXML
     public void initialize()
     {
-        File userDataXml = new File("./src/main/java/database/user-data.xml");
-        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-        try {
-            DocumentBuilder builder = builderFactory.newDocumentBuilder();
-            org.w3c.dom.Document userData = builder.parse(userDataXml);
-            userData.getDocumentElement().normalize();
-
-            NodeList nodeList = userData.getElementsByTagName("user");
-            this.userId = ((Element) nodeList.item(0)).getElementsByTagName("_id").item(0).getTextContent();
-            addFlashcards();
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void addFlashcards()
-    {
-        flashcardsBox.setHgap(25);
-        flashcardsBox.setAlignment(Pos.TOP_CENTER);
-
-        File userDataXml = new File("./src/main/java/database/user-data.xml");
-        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-        try {
-            DocumentBuilder builder = builderFactory.newDocumentBuilder();
-            org.w3c.dom.Document userData = builder.parse(userDataXml);
-            userData.getDocumentElement().normalize();
-
-            NodeList nodeList = userData.getElementsByTagName("user");
-            NodeList flashcardsList = ((Element) nodeList.item(0)).getElementsByTagName("flashcards").item(0).getChildNodes();
-            int flashcardsListLength = flashcardsList.getLength();
-            for (int ind = 0; ind < flashcardsListLength; ind++) {
-                Element flashcardXml = (Element) flashcardsList.item(ind);
-                String flashcardTitle = flashcardXml.getElementsByTagName("title").item(0).getTextContent();
-                String flashcardDescription = flashcardXml.getElementsByTagName("description").item(0).getTextContent();
-                FlowPane flashcard = setFlashcardInformation(flashcardTitle, flashcardDescription);
-                flashcardsBox.getChildren().add(flashcard);
-            }
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public FlowPane setFlashcardInformation(String flashcardTitle, String flashcardDescription)
-    {
-        FlowPane flashcard = new FlowPane();
-        flashcard.setAlignment(Pos.TOP_CENTER);
-
-        Label title = new Label(flashcardTitle);
-        title.getStyleClass().add("flashcard__title");
-        Label description = new Label(flashcardDescription);
-        description.getStyleClass().add("flashcard__description");
-        description.visibleProperty().set(false);
-
-        ToolBar flashcardToolBar = new ToolBar();
-        flashcardToolBar.getStyleClass().add("flashcardToolBar");
-        Button removeBtn = new Button();
-        removeBtn.getStyleClass().add("flashcard__remove-btn");
-        Button shareBtn = new Button();
-        shareBtn.getStyleClass().add("flashcard__share-btn");
-        Button editBtn = new Button();
-        editBtn.getStyleClass().add("flashcard__edit-btn");
-
-        flashcardToolBar.getItems().add(removeBtn);
-        flashcardToolBar.getItems().add(shareBtn);
-        flashcardToolBar.getItems().add(editBtn);
-
-        flashcard.getChildren().addAll(title, description, flashcardToolBar);
-
-        RotateTransition rotateTransition = new RotateTransition();
-        rotateTransition.setDuration(Duration.millis(1000));
-        rotateTransition.setAxis(Rotate.Y_AXIS);
-        rotateTransition.setCycleCount(1);
-        rotateTransition.setNode(flashcard);
-
-        flashcard.getStyleClass().add("green-bg");
-        flashcard.getStyleClass().add("flashcard");
-        flashcard.onMouseClickedProperty().set(mouseEvent -> {
-            rotateTransition.play();
-            if (title.visibleProperty().get()) {
-                title.visibleProperty().set(false);
-                description.visibleProperty().set(true);
-                flashcardToolBar.visibleProperty().set(false);
-            } else {
-                title.visibleProperty().set(true);
-                description.visibleProperty().set(false);
-                flashcardToolBar.visibleProperty().set(true);
-            }
-            if (flashcard.getStyleClass().contains("green-bg")) {
-                flashcard.getStyleClass().remove("green-bg");
-                flashcard.getStyleClass().add("red-bg");
-            } else {
-                flashcard.getStyleClass().remove("red-bg");
-                flashcard.getStyleClass().add("green-bg");
-            }
-        });
-
-        return flashcard;
+        setFlashcardBoxProperties();
+        this.xmlReader = new XMLReader(USER_DATA_PATH, "user");
+        this.userId = this.xmlReader.getTagTextContent(this.xmlReader.userDataRootNode, "_id");
+        addFlashcards();
     }
 
     @FXML
     public void goToNotificationsScene(MouseEvent mouseEvent) throws IOException
     {
-        FXMLLoader notificationsFXML = new FXMLLoader(Paths.get("./src/main/java/fxml/notifications.fxml").toUri().toURL());
-        switchScene(mouseEvent, notificationsFXML);
+        switchScene(mouseEvent, NOTIFICATIONS_SCENE_PATH);
     }
 
     @FXML
@@ -152,5 +56,26 @@ public class HomeController extends Controller
     {
         Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
         window.close();
+    }
+
+    public void setFlashcardBoxProperties()
+    {
+        this.flashcardBox.setHgap(25);
+        this.flashcardBox.setAlignment(Pos.TOP_CENTER);
+    }
+
+    public void addFlashcards()
+    {
+        try {
+            Element userElement = (Element) this.xmlReader.userDataDocument.getElementsByTagName("user").item(0);
+            Element flashcardsElements = (Element) userElement.getElementsByTagName("flashcards").item(0);
+            int flashcardListLength = flashcardsElements.getElementsByTagName("flashcard").getLength();
+            for (int i = 0; i < flashcardListLength; i++) {
+                AnchorPane flashcard = FXMLLoader.load(Paths.get(FLASHCARD_PATH).toUri().toURL());
+                flashcardBox.getChildren().add(flashcard);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
