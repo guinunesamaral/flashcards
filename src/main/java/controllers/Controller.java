45dc1167742d2af2534f11c4b29b5e48f2b8750e
guilherme nunes <guinucode@protonmail.com>
2021-04-18 14:29:04 -0300

    Moved the FXMLloader logic to the Controller class, and added String constants containing the paths to other scenes in the Login and Register controllers

--- src/main/java/controllers/Controller.java
+++ src/main/java/controllers/Controller.java
@@ -7,11 +7,13 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class Controller
 {
-    public void switchScene(MouseEvent mouseEvent, FXMLLoader sceneFXML) throws IOException
+    public void switchScene(MouseEvent mouseEvent, String sceneFxmlPath) throws IOException
     {
+        FXMLLoader sceneFXML = new FXMLLoader(Paths.get(sceneFxmlPath).toUri().toURL());
         Scene scene = new Scene(sceneFXML.load());
         Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
         window.setScene(scene);
