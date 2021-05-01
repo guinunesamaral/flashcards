faf2b7f5d222038abfd104a2b0de54176c9290da
guilherme nunes <guinucode@protonmail.com>
2021-05-01 03:32:46 -0300

    Created a package that contains an utilitarie class for the flashcard abstraction

---
+++ src/main/java/utilities/Flashcard.java
@@ -0,0 +1,11 @@
+package utilities;
+
+public class Flashcard
+{
+    public String description;
+
+    public Flashcard(String description)
+    {
+        this.description = description;
+    }
+}
