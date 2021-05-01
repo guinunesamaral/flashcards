d1221a45bb1d31117ac5135b5a0ae585ac3b60ff
guilherme nunes <guinucode@protonmail.com>
2021-05-01 03:31:53 -0300

    Added methods to return a list of Flashcard to be read by the StudyController

--- src/main/java/database/UserDataReader.java
+++ src/main/java/database/UserDataReader.java
@@ -4,17 +4,18 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
+import utilities.Flashcard;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class UserDataReader
 {
     public static final String USER_DATA = "./src/main/java/userdata/user-data.xml";
-
     public Document userDataDocument;
     public Node userDataRootNode;
 
@@ -46,5 +47,20 @@ public class UserDataReader
     public Element getTagElements(Node origin, String tagName, int index)
     {
         return (Element) ((Element) origin).getElementsByTagName(tagName).item(index);
+    }
+
+    public ArrayList<Flashcard> getFlashcards()
+    {
+        ArrayList<Flashcard> flashcards = new ArrayList<>();
+        Element flashcardsElement = getTagElements(this.userDataRootNode, "flashcards", 0);
+        int numberOfFlashcards = flashcardsElement.getElementsByTagName("flashcard").getLength();
+
+        for (int index = 0; index < numberOfFlashcards; index++)
+        {
+            Element flashcard = getTagElements(flashcardsElement, "flashcard", index);
+            String description = getTagElements(flashcard, "description", 0).getTextContent();
+            flashcards.add(new Flashcard(description));
+        }
+        return flashcards;
     }
 }
