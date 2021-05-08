9176f48b5cbd05b570cfd7110d986ca0c6390587
guilherme nunes <guinucode@protonmail.com>
2021-05-08 15:38:11 -0300

    This package contains classes such as User and Flashcard to help in the flashcard operations, and the Score class only store the values of hard and easy answers from the user

--- src/main/java/utilities/Flashcard.java
+++ src/main/java/utilities/Flashcard.java
@@ -1,11 +1,71 @@
 package utilities;
 
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+
 public class Flashcard
 {
-    public String description;
+    private final ObjectId id;
+    private ArrayList<ObjectId> users;
+    private String front;
+    private String back;
 
-    public Flashcard(String description)
+    public Flashcard(Flashcard flashcard)
     {
-        this.description = description;
+        this.id = flashcard.getId();
+        this.users = flashcard.getUsers();
+        this.front = flashcard.getFront();
+        this.back = flashcard.getBack();
+    }
+
+    public Flashcard(ObjectId id,
+                     String front,
+                     String back)
+    {
+        this.id = id;
+        this.front = front;
+        this.back = back;
+    }
+
+    public Flashcard(ObjectId id,
+                     ArrayList<ObjectId> users,
+                     String front,
+                     String back)
+    {
+        this.id = id;
+        this.users = users;
+        this.front = front;
+        this.back = back;
+    }
+
+    public ObjectId getId()
+    {
+        return this.id;
+    }
+
+    public ArrayList<ObjectId> getUsers()
+    {
+        return this.users;
+    }
+
+    public String getFront()
+    {
+        return front;
+    }
+
+    public void setFront(String front)
+    {
+        this.front = front;
+    }
+
+    public String getBack()
+    {
+        return back;
+    }
+
+    public void setBack(String back)
+    {
+        this.back = back;
     }
 }
---
+++ src/main/java/utilities/User.java
@@ -0,0 +1,90 @@
+package utilities;
+
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+
+public class User
+{
+    private static User user;
+    private static ObjectId id;
+    private static String name;
+    private static String email;
+    private static String password;
+    private static ArrayList<Flashcard> flashcards;
+    private static ArrayList<Flashcard> receivedFlashcards;
+
+    private User()
+    {
+        flashcards = new ArrayList<>();
+        receivedFlashcards = new ArrayList<>();
+    }
+
+    public static User getInstance()
+    {
+        if (user == null) {
+            user = new User();
+        }
+        return user;
+    }
+
+    public ObjectId getId()
+    {
+        return id;
+    }
+
+    public void setId(ObjectId id)
+    {
+        User.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        User.name = name;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        User.email = email;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        User.password = password;
+    }
+
+    public void addFlashcard(Flashcard flashcard)
+    {
+        User.flashcards.add(flashcard);
+    }
+
+    public ArrayList<Flashcard> getFlashcards()
+    {
+        return flashcards;
+    }
+
+    public ArrayList<Flashcard> getReceivedFlashcards()
+    {
+        return receivedFlashcards;
+    }
+
+    public void addReceivedFlashcards(Flashcard receivedFlashcard)
+    {
+        User.receivedFlashcards.add(receivedFlashcard);
+    }
+}
